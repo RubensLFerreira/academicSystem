2e22524c0512a8ac50580c0bc83d91c66b2c762f
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-08-15 12:43:11 -0300

    chore: corrgindo bugs nas rests

--- src/controller/disciplina/DisciplinaController.js
+++ src/controller/disciplina/DisciplinaController.js
@@ -10,47 +10,96 @@ export default {
       return res.status(400).json({ message: error.message });
     }
   },
-  
+
   async createDisciplina(req, res) {
-    const { nome, ch } = req.body;
-    const { id_semestre } = req.params;
-    const { id_curso } = req.params;
     const { id_professor } = req.params;
+    const { id_semestre } = req.params;
+    const { nome, ch } = req.body;
 
     try {
-      const semestre = await prisma.semestre.findUnique({
-        where: { id: Number(id_semestre) },
-      });
-      const curso = await prisma.curso.findUnique({
-        where: { id: Number(id_curso) },
-      });
-      const professor = await prisma.professor.findUnique({
+      const professor = await prisma.professor.findFirst({
         where: { id: Number(id_professor) },
       });
-  
-      if (!semestre || !curso || !professor) {
+
+      const semestre = await prisma.semestre.findFirst({
+        where: { id: Number(id_semestre) },
+      });
+
+      if (!professor || !semestre) {
         return res
           .status(404)
-          .json({ message: "Não foi possível encontrar curso, semestre, prof" });
+          .json({ message: "Este professor/semestre não existe!" });
       }
-  
+
       const disciplina = await prisma.disciplina.create({
         data: {
           nome,
           ch,
-          semestreid: semestre.id,
-          cursoid: curso.id,
           professorid: professor.id,
+          semestreid: semestre.id,
         },
         include: {
-          curso: true,
           professor: true,
           semestre: true,
         },
       });
-      return req.status(201).json(disciplina);
+
+      return res.status(201).json(disciplina);
     } catch (error) {
       return res.status(400).json({ message: error.message });
+    }
+  },
+
+  async updateDisciplina(req, res) {
+    const { id } = req.params;
+    const { nome, ch, professorid, semestreid } = req.body;
+
+    try {
+      const disciplina = await prisma.disciplina.findFirst({
+        where: { id: Number(id) },
+      });
+
+      if (!disciplina) {
+        return res.status(404).json({ message: "Esta disciplina não existe!" });
+      }
+
+      disciplina = await prisma.disciplina.update({
+        where: {
+          id: Number(id),
+        },
+        data: {
+          nome,
+          ch,
+          professorid,
+          semestreid,
+        },
+      });
+
+      return res
+        .status(200)
+        .json({ message: "Disciplina atualizada com sucesso!" });
+    } catch (error) {
+      return res.json({ message: error.message });
+    }
+  },
+
+  async deleteDisciplina(req, res) {
+    const { id } = req.params;
+
+    try {
+      const disciplina = await prisma.disciplina.findFirst({
+        where: { id: Number(id) },
+      });
+
+      if (!disciplina) {
+        return res.status(404).json({ message: "Esta disciplina não existe!" });
+      }
+
+      return res
+        .status(200)
+        .json({ message: "Disciplina deleta com sucesso!" });
+    } catch (error) {
+      return res.json({ message: error.message });
     }
   },
 };
